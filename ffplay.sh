a5b196905cd7eebdc6304cd296ee4b98c16f2bc4
m78452396 <m78452396@gmail.com>
2025-05-28 13:30:38 -0300

    Add files via upload

---
+++ ffplay.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# version	: 0.1
+# last update	: 05/27/2025
+
+ffplay "rtsp://admin:F8770009@192.168.1.15:554/cam/realmonitor?channel=1&subtype=0"	\
+		-window_title "ffplay (192.168.1.15)"					\
+		-rtsp_transport tcp							\
+		-x 1049									\
+		-preset:v ultrafast							\
+		-threads 1								\
+		-an
