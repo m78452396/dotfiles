7a615b6541e13e168747bb0d34a30a17698009b8
Marcos Gabriel <m78452396@gmail.com>
2025-05-30 13:02:43 -0300

    Update randr-m.sh

--- randr-m.sh
+++ randr-m.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # version	: 0.1
-# last update	: 05/27/2025
+# last update	: 05/28/2025
 
 wlr-randr --output HDMI-A-1 --preferred --pos 0,0 && sleep 0.5s && wlr-randr --output HDMI-A-1 --on && sleep 0.5s && wlr-randr --output eDP-1 --off
